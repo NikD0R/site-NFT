3ed73b7e599f11cfe5949685c9726bfa6180df6e
Nikita Doroshenko <nikitokd05@gmail.com>
2023-01-28 00:54:59 +0200

    added experience section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-  
-})
+  $('.experience-slider').slick({
+    dots: false,
+    variableWidth: true,
+    draggable: false,
+    prevArrow: '<img class="arrow arrow-left" src="images/icons/arrow-left.svg" alt="left arrow of slider">',
+    nextArrow: '<img class="arrow arrow-right" src="images/icons/arrow-right.svg" alt="right arrow of slider">',
+  });
+});
