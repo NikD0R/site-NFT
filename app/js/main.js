508e8ee82d2e21d914cacb2fb06a417f37d9f4f2
Nikita Doroshenko <nikitokd05@gmail.com>
2023-02-21 18:08:49 +0200

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,21 @@
+// const { event } = require("jquery");
+
 $(function(){
+
+  $('.menu a, .logo, .footer-top__list a').on('click', function(event) {
+    event.preventDefault();
+
+    var id = $(this).attr('href'),
+
+    top = $(id).offset().top;
+
+    $('body, html').animate({scrollTop: top}, 1500);
+  });
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');  
+  });
+
   $('.experience-slider').slick({
     dots: false,
     variableWidth: true,
@@ -7,6 +24,19 @@ $(function(){
     autoplaySpeed: 2000,
     prevArrow: '<img class="arrow arrow-left" src="images/icons/arrow-left.svg" alt="left arrow of slider">',
     nextArrow: '<img class="arrow arrow-right" src="images/icons/arrow-right.svg" alt="right arrow of slider">',
+    responsive: [
+      {
+        breakpoint: 950,
+        settings: {
+          draggable: true,
+        },
+        breakpoint: 385,
+        settings: {
+          arrows: false,
+          draggable: true,
+        }
+      }
+    ]
   });
 
   $('.team__filter-btn').on('click', function () {
@@ -15,22 +45,22 @@ $(function(){
   });
 
   $('.team__filter-btn--team-1').on('click',function(){
-    $('.team__inner--first').css('display', 'flex');
-    // $('.team__inner--first').show('slow');
+    // $('.team__inner--first').css('display', 'flex');
+    $('.team__inner--first').show('slow');
     $('.team__inner--second').hide();
     $('.team__inner--third').hide();
   });
 
   $('.team__filter-btn--team-2').on('click', function() {
-    $('.team__inner--second').css('display', 'flex');
-    // $('.team__inner--second').show('slow');
+    // $('.team__inner--second').css('display', 'flex');
+    $('.team__inner--second').show('slow');
     $('.team__inner--first').hide();
     $('.team__inner--third').hide();
  });
 
  $('.team__filter-btn--team-3').on('click', function() {
-   $('.team__inner--third').css('display', 'flex');
-    // $('.team__inner--third').show('slow');
+  //  $('.team__inner--third').css('display', 'flex');
+    $('.team__inner--third').show('slow');
     $('.team__inner--first').hide();
     $('.team__inner--second').hide();
  });
